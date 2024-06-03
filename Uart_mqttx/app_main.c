76f8252d173f9b6960495f357a574b6c192c4eac
anshasija12 <78747252+anshasija12@users.noreply.github.com>
2024-06-03 14:39:39 +0530

    Add files via upload

---
+++ Uart_mqttx/app_main.c
@@ -0,0 +1,192 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_err.h"
+
+#include "esp_system.h"
+#include "driver/uart.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+
+void uart_init(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,//UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_2, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+
+static const char *TAG = "MQTT";
+#define EXAMPLE_ESP_WIFI_SSID "Galaxy A51DC13"
+#define EXAMPLE_ESP_WIFI_PASS "qwertypoiu"
+#define MAX_RETRY 10
+static int retry_cnt = 0;
+
+
+#define topic "esp32/data"
+
+uint32_t MQTT_CONNECTED = 0;
+
+static void mqtt_app_start(void);
+
+static esp_err_t wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        esp_wifi_connect();
+        ESP_LOGI(TAG, "Trying to connect with Wi-Fi\n");
+        break;
+
+    case WIFI_EVENT_STA_CONNECTED:
+        ESP_LOGI(TAG, "Wi-Fi connected\n");
+        mqtt_app_start();
+        break;
+
+
+    case WIFI_EVENT_STA_DISCONNECTED:
+        ESP_LOGI(TAG, "disconnected: Retrying Wi-Fi\n");
+        if (retry_cnt++ < MAX_RETRY)
+        {
+            esp_wifi_connect();
+        }
+        else
+            ESP_LOGI(TAG, "Max Retry Failed: Wi-Fi Connection\n");
+        break;
+
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+void wifi_init(void)
+{
+    esp_event_loop_create_default();
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
+
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    esp_netif_init();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    esp_wifi_start();
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        MQTT_CONNECTED = 1;
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        MQTT_CONNECTED = 0;
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+esp_mqtt_client_handle_t client = NULL;
+
+void Publisher_Task(void)
+{
+    uint8_t* DATA = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    uart_read_bytes(UART_NUM_2, DATA, RX_BUF_SIZE, 500/ portTICK_PERIOD_MS);
+    ESP_LOGI("RX_TASK", "Read bytes : %s", DATA);
+       
+    if (MQTT_CONNECTED)
+    { 
+        esp_mqtt_client_publish(client, topic, (const char*)DATA, 0, 0, 0);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "MQTT Not connected");
+    }
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    free(DATA);
+    
+}
+
+static void mqtt_app_start(void)
+{
+    ESP_LOGI(TAG, "STARTING MQTT");
+    esp_mqtt_client_config_t mqttConfig = {
+        .broker.address.uri = "mqtt://broker.emqx.io:1883"};
+
+    client = esp_mqtt_client_init(&mqttConfig);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(client);
+    //xTaskCreate(Publisher_Task, "Publisher_Task", 1024 * 5, NULL, 5, NULL);
+    ESP_LOGI(TAG, "MQTT Publisher_Task is up and running\n");
+}
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    ESP_ERROR_CHECK(ret);
+    wifi_init();
+    uart_init();
+
+    while(1)
+    {
+        vTaskDelay(1);
+        Publisher_Task();
+    }
+}
+
+
